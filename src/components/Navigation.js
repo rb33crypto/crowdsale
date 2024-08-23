051a3d327e7f249700342aac44bb0460313eb4ad
rb33crypto <rb33crypto@gmail.com>
2024-08-23 14:54:37 -0700

    Nav Bar

--- src/components/Navigation.js
+++ src/components/Navigation.js
@@ -3,18 +3,21 @@ import Navbar from 'react-bootstrap/Navbar';
 import logo from '../logo.png';
 
 const Navigation = () => {
-    return (
-        <Navbar className='my-3'>
-            <img
-                alt="logo"
-                src={logo}
-                width="40"
-                height="40"
-                className="d-inline-block align-top mx-3"
-            />
-            <Navbar.Brand href="#">DApp ICO Crowdsale</Navbar.Brand>
-        </Navbar>
-    );
+  return(
+  
+      <Navbar>
+        <img
+          alt= "logo"
+          src= {logo}
+          width= "40"
+          height= "40"
+          className= "d-inline-block align-top mx-3"
+          />
+          <Navbar.Brand href="#"> DAPP ICO Crowdsale</Navbar.Brand>
+      </Navbar>
+   
+    )
 }
 
 export default Navigation;
+ 
